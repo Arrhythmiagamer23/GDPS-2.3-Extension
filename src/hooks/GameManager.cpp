604da8c5589ea8a847b92ec0571254cfce7381db
KDSealock <158773839+Arrhythmiagamer23@users.noreply.github.com>
2026-07-04 21:49:09 -0400

    Refactor GameManager for background and ground loading
    Refactor GameManager to manage background and ground loading with new logic.

--- src/hooks/GameManager.cpp
+++ src/hooks/GameManager.cpp
@@ -1,112 +1,94 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/GameManager.hpp>
-#include "../utils/Utils.hpp"
-#include "../utils/IconUtils.hpp"
 
 using namespace geode::prelude;
 
-enum class CustomIcons
-{
-    Cube = 383,
-    Ship = 70,
-    Ball = 82,
-    UFO = 62,
-    Wave = 51,
-    Swing = 5,
-    Jetpack = 1
-};
-
 class $modify(OdysseyGameManager, GameManager)
 {
-    void firstLoad()
-    {
-        GameManager::firstLoad();
-    }
-
-    int countForType(IconType icon)
-    {
-        switch (icon)
-        {
-        case IconType::Cube:
-            return 485 + (int)CustomIcons::Cube;
-        case IconType::Ship:
-            return 169 + (int)CustomIcons::Ship;
-        case IconType::Ball:
-            return 118 + (int)CustomIcons::Ball;
-        case IconType::Ufo:
-            return 149 + (int)CustomIcons::UFO;
-        case IconType::Wave:
-            return 96 + (int)CustomIcons::Wave;
-        case IconType::Robot:
-            return 68;
-        case IconType::Spider:
-            return 69;
-        case IconType::Swing:
-            return 43 + (int)CustomIcons::Swing;
-         case IconType::Jetpack:
-            return 8 + (int)CustomIcons::Jetpack;
-        case IconType::Special:
-            return 7;
-        case IconType::DeathEffect:
-            return 20;
-        case IconType::ShipFire:
-            return 6;
-        case IconType::Item:
-            return 21;
-        default:
-            //  Yes, this solution is terrible, but I don't know how to patch bytes.
-            return GameManager::countForType(icon);
-        }
-    }
-
-    bool isIconUnlocked(int id, IconType type)
-    {
-        if (IconUtils::isIconCustom(id, type))
-        {
-            if (IconUtils::isCustomVehicle(type))
-            {
-                auto obj = get()->m_valueKeeper->valueForKey(IconUtils::getItemKey(id, IconUtils::iconTypeToInt(type)));
-                return (id == 1) ? true : obj->intValue() == 1;
-            }
-            else
-            {
-                return GameManager::isIconUnlocked(id, type);
-            }
-        }
-
-        if (type == IconType::Item)
-            return GameManager::isIconUnlocked(id, type);
-
-        return true;
-    }
-
-    bool isColorUnlocked(int id, UnlockType type)
-    {
-        return true;
-    }
-
-    void dataLoaded(DS_Dictionary *dict)
-    {
-        GameManager::dataLoaded(dict);
-    }
-
-    gd::string sheetNameForIcon(int iconID, int iconType)
-    {
-        if (iconType >= 900)
-        {
-            const char *gamemode[4] = {"boat", "drone", "slider", "minecart"};
-
-            return fmt::format("icons/{}_{:02}", gamemode[iconType - 901], iconID);
-        }
-
-        auto ret = GameManager::sheetNameForIcon(iconID, iconType);
-        return ret;
+    struct Fields {
+        int m_loadedBGIdx = 0;
+        int m_loadedGIdx = 0;
     };
+    
+    bool init() {
+        if (!GameManager::init()) {
+            return false;
+        }
+        
+        return true;
+    }
+    
+    void GameManager::loadBackground(int backID)
+    {
+	    int bgID = backID;
+	    if (190 < backID) {
+            bgID = 191;
+        }
+	    if (backID < 1) {
+        bgID = 1;
+        }
+        if (bgID != m_loadedBGIdx) {
+		    CCString* bgStr;
+		    CCTextureCache* pTextureCache = CCTextureCache::sharedTextureCache();
+		    if (this->m_loadedBGIdx != 0)
+		    {
+			    bgStr = CCString::createWithFormat("game_bg_%02d_001.png", bgID);
+			    // pTextureCache->removeTextureForKey(bgStr->getCString);
+		    }
+		    bgStr = CCString::createWithFormat("game_bg_%02d_001.png", bgID);
+		    pTextureCache->addImage(bgStr->getCString());
+		    this->m_loadedBGIdx = bgID;
+        }
+    }
 
-    CCTexture2D* loadIcon(int id, int type, int requestID) {
-        if (type>=900) type=1;
+    void GameManager::loadGround(int gID)
+    {
+        int groundID = gID;
+        if (40 < gID) {
+            groundID = 41;
+        }
+        if (gID < 1) {
+            groundID = 1;
+        }
+        if (groundID != this->m_loadedGIdx)
+        {
+            CCString* groundStr;
+            CCTextureCache* pTextureCache = CCTextureCache::sharedTextureCache();
+            if (this->m_loadedGIdx != 0)
+            {
+                groundStr = CCString::createWithFormat("groundSquare_%02d_001.png", groundID);
+                // MISSING CC FUNCTION: pTextureCache->removeTextureForKey(groundStr->getCString);
+            }
+            groundStr = CCString::createWithFormat("groundSquare_%02d_001.png", groundID);
+            pTextureCache->addImage(groundStr->getCString());
+            this->m_loadedGIdx = groundID;
+        }
+    }
 
-        CCTexture2D* ret = GameManager::loadIcon(id, type, requestID);
-        return ret;
+    // m_id in these 2 functions is a temporary variable used as a failsafe for overshooting on ground and background ids
+    char const* GameManager::getBGTexture(int bgID)
+    {
+        int m_id = bgID;
+        if (190 < bgID) {
+            m_id = 191;
+        }
+        if (bgID < 1) {
+            m_id = 1;
+        }
+	    this->loadBackground(m_id);
+	    return CCString::createWithFormat("game_bg_%02d_001.png", m_id)->getCString();
+    }
+
+    char const* GameManager::getGTexture(int gID)
+    {
+        int m_id = gID;
+        if (40 < gID) {
+            m_id = 41;
+        }
+        if (gID < 1) {
+            m_id = 1;
+        }
+        this->loadGround(m_id);
+        return CCString::createWithFormat("groundSquare_%02d_001.png", m_id)->getCString();
     }
 };
