09bd3d6a9ea82c685f6729d833314f78164aa1a3
KDSealock <158773839+Arrhythmiagamer23@users.noreply.github.com>
2026-08-30 12:29:54 -0400

    Delete include/cosmic_clones directory

--- include/cosmic_clones/API.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <Geode/loader/Dispatch.hpp>
-
-namespace cosmic_clones {
-    inline void setClonesVisible(bool visible) GEODE_EVENT_EXPORT_ID_NORES(&setClonesVisible, (visible), "km7dev.cosmic_clones/setClonesVisible");
-}
